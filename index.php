27e1df0f6db66b9456fd3211193f54d7aa178d46
unknown <jean05lucca@gmail.com>
2024-11-06 21:34:37 -0300

    Trabalho do Leandro

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="style.css" href="style.css">
+</head>
+<body>
+	<h2>Cadastro Agronomia</h2>
+	<a href="html/tabelaProp.php"><button>Propriedade</button></a>
+	<br><br>
+	<a href="html/tabelaCult.php"><button>Culturas</button></a>
+	<br><br>
+	<a href="html/tabelaEquip.php"><button>Equipamentos</button></a>
+	<br><br>
+</body>
+</html>
